d6e217850f62634afab1c577564385b56d831479
scambra <sergio@entrecables.com>
2011-11-09 17:57:35 +0100

    add localization

--- lib/record_select/extensions/localization.rb
+++ lib/record_select/extensions/localization.rb
@@ -2,7 +2,12 @@
 # to localize RS, you need to override this method and route it to your
 # own system.
 class Object
-  def rs_(string_to_localize, *args)
-    args.empty? ? string_to_localize : (sprintf string_to_localize, *args)
+  def rs_(key, options = {})
+    unless key.blank?
+      text = I18n.translate "#{key}", {:scope => [:record_select], :default => key.is_a?(String) ? key : key.to_s.titleize}.merge(options)
+      # text = nil if text.include?('translation missing:')
+    end
+    text ||= key 
+    text
   end
 end
